c17cf14362708639df3b875c0787c6c872a6eba9
John Edwards <edwards.aus@hotmail.com>
2020-12-30 11:10:05 +1100

    added details to README, added the script and the requirements file to free ctmonitor monitoring

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ctmonitor
 Simple script(s) to monitor the SSL certificate transparency logs via ctstream
+
+## Setup
+Run the following:
+```
+python3 -m pip install -r requirements.txt
+```
+
+## Usage
+Run script without args to see usage information.
---
+++ ctmonitor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DEFAULT_CERTSTREAM_BIN="/usr/local/bin/certstream"
+USAGE="
+[-] $0 <keywords/grep-pattern> [certstream_bin=$DEFAULT_CERTSTREAM_BIN]
+
+Script will look for any SSL certifications containing specific keywords/grep 
+pattern.
+
+Example: 
+    To search for all certs containing the keyword 'webmail' OR 'disk'
+        $0 'webmail|disk'
+"
+if [ $# -lt 1 ]; then
+    echo "$USAGE"
+    exit 1
+fi
+grep_pattern="$1"
+certstream_bin=${2:-"$DEFAULT_CERTSTREAM_BIN"}
+
+if [ ! -f "$certstream_bin" ]; then
+    echo "[-] Certstream binary not found at location: $certstream_bin"
+    exit 1
+fi
+
+# Launch the certstream and start looking for the pattern
+certstream --disable-colors --full 2>&1 | grep -iE "$grep_pattern"
